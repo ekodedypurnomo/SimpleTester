04fb9f145aa39b360f159b61e8243edffd8480fd
Eko Dedy Purnomo <eko.dedy.purnomo@gmail.com>
2015-04-13 14:32:57 +0700

    Update example.php

--- example.php
+++ example.php
@@ -1,20 +1,24 @@
 <?php
 
+// include the class
 require 'SimpleTester.php';
 
+// create object of it
 $tester = new SimpleTester;
+// then use it just like:
 $tester->subtest('Test SimpleTester class', function($subtest) use($tester){
-    $subtest->test('must have a name', property_exists($tester, 'name'));
-    $subtest->test('must have a templateReport', property_exists($tester, 'templateReport'));
+	// test($testName@string, $assertion@boolean)
+	$subtest->test('must have a name', property_exists($tester, 'name'));
+    	$subtest->test('must have a templateReport', property_exists($tester, 'templateReport'));
 });
 $tester->subtest('Test SimpleSubtester class', function($subtest){
-    $subtest->test('must have a name', empty($subtest->name) === false);
-    $subtest->test('must have a tester', empty($subtest->tester) === false);
+    	$subtest->test('must have a name', empty($subtest->name) === false);
+	$subtest->test('must have a tester', empty($subtest->tester) === false);
 });
 
-// echo "<pre>";
-// var_export($tester->result());
-
+// how about catch the result?
+// dont worry, it so simple, use var_dump or var_export to see the result
+// then manipulate it, done.
 echo 'Testing class SimpleTester within result:<br/>';
 $no = 0;
 foreach ($tester->result() as $subtest => $tests) {
