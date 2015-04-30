6ef758123e4b4878588857272dcdc03bec6e3424
eko.dedy.purnomo <eko.dedy.purnomo@gmail.com>
2015-04-30 17:23:01 +0700

    Add script documentation for SimpleTester

--- SimpleTester.php
+++ SimpleTester.php
@@ -1,19 +1,60 @@
 <?php
 
+/**
+ * @package SimpleTester
+ * @author Eko Dedy Purnomo <eko.dedy.purnomo@gmail.com>
+ * @version 0.1.0
+ */
+
 require __DIR__ . DIRECTORY_SEPARATOR.'SimpleSubtester.php';
 
+/**
+ * SimpleTester is a simple unit test class.
+ */
 class SimpleTester{
 
-    public $templateReport = array();
-
+    /**
+     * $name Name of test.
+     * @var null
+     */
     public $name = null;
 
+    /**
+     * $subtests Subtests will be saved on it.
+     * @var array
+     */
     protected $subtests = array();
 
+    /**
+     * Create new SimpleTest.
+     * Example:
+     *     
+     *     $tester = new SimpleTest('somename');
+     *     $tester->subtest('Test for module 1', function($subtest, $me){
+     *         $subtest->test('Should have $name property', property_exists($me, 'name'));
+     *     });
+     * 
+     * @param string $name Name of test.
+     */
     function __construct($name = null){
         $this->name = $name;
     }
 
+    /**
+     * Get subtest object by name.
+     * Example:
+     * 
+     *     $tester = new SimpleTest('somename');
+     *     $tester->subtest('Test for module 1', function($subtest, $me){
+     *         $subtest->test('Should have $name property', property_exists($me, 'name'));
+     *     });
+     *     $subtest = $tester->getSubtest('Test for module 1');
+     *     $result = $subtest->result();
+     *     print_r($result);
+     * 
+     * @param  string           $subtestName    Subtest name
+     * @return SimpleSubtester                  Subtest object, `null` if not found.
+     */
     function getSubtest($subtestName = null){
         if(!is_array($this->subtests)) $this->subtests = array();
         
@@ -28,25 +69,39 @@ class SimpleTester{
         }
     }
 
+    /**
+     * Add new subtest to the tester.
+     * Subtest are part of this to be a result of some tests.
+     * 
+     * @param SimpleSubtester|null $subtest Subtester object to be added in.
+     */
     function addSubtest(SimpleSubtester $subtest = null){
         if(!is_array($this->subtests)) $this->subtests = array();
         $this->subtests[] = $subtest;
+        return $this;
     }
 
     /**
-     * [subtest description]
-     *
-     *
-     *      $tester = new Tester;
-     *      $tester->subtest('testing module x', function($subtest){
+     * Create subtest and sun the test for it.
+     * Tests are run under callback on the 2nd parameter, make it easy to use.
+     * 
+     *      $tester = new SimpleTester();
+     *      $tester->subtest('testing module 1', function($subtest, $me){
      *          $subtest->test('testing argument1', true);
      *          $subtest->test('testing argument2', true);
-     *          return false; // false make ignore the testing to assert in report
+     *      });
+     *      $tester->subtest('testing module 2', function($subtest, $me){
+     *          $subtest->test('testing argument1', true);
+     *          $subtest->test('testing argument2', true);
+     *      });
+     *      // you cann add new tests in next time, by pass same subtest name
+     *      $tester->subtest('testing module 1', ($subtest, $me){
+     *          $subtest->test('testing argument3', true);
      *      });
      * 
-     * @param  integer $subtestName [description]
-     * @param  [type]  $callback    [description]
-     * @return [type]               [description]
+     * @param  string   $subtestName Subtest name.
+     * @param  callable $callback    A function to process the test, return `false` to prevent tests added to result. 
+     * @return SimpleSubtester
      */
     function subtest($subtestName = null, $callback = null){
         $subtest = $this->getSubtest($subtestName);
@@ -58,34 +113,15 @@ class SimpleTester{
             $this->addSubtest($subtest);
         }
 
-        call_user_func_array($callback, array($subtest, $this));
+        if(is_callable($callback)) call_user_func_array($callback, array($subtest, $this));
+
         return $subtest;
     }
 
     /**
-     * [test description]
-     *
-     *      $test = $tester->test('argue 1');
-     *      $test->id; // id test
-     *      $test->type; // 'test','sub','module'
-     *      $test->result(); // array('type'=>'test','result'=>boolean)
-     *      // if type is module
-     *      $test->subs; array();
-     *      // if type is sub
-     *      $test->tests; array();
-     *      
-     * @param  [type]  $testName    [description]
-     * @param  boolean $assertation [description]
-     * @return [type]               [description]
+     * Get the result of each subtest.
+     * @return array Array Subtest name and test result pairs
      */
-    function test($testName, $assertation = false, $subtest){
-        if(!$this->getSubtest($subtest)){
-            $this->addSubtest($subtest);
-        }
-
-        $subtest->addTest($testName, $assertation);
-    }
-
     function result(){
         if(!is_array($this->subtests)) $this->subtests = array();
         $result = array();
@@ -95,5 +131,4 @@ class SimpleTester{
         return $result;
     }
 
-    function report(){}
 }
--- example.php
+++ example.php
@@ -2,19 +2,21 @@
 
 require 'SimpleTester.php';
 
+// begin the test
 $tester = new SimpleTester;
+// test grup 1
 $tester->subtest('Test SimpleTester class', function($subtest) use($tester){
     $subtest->test('must have a name', property_exists($tester, 'name'));
     $subtest->test('must have a templateReport', property_exists($tester, 'templateReport'));
 });
+// test grup 2
 $tester->subtest('Test SimpleSubtester class', function($subtest){
     $subtest->test('must have a name', empty($subtest->name) === false);
     $subtest->test('must have a tester', empty($subtest->tester) === false);
 });
 
-// echo "<pre>";
-// var_export($tester->result());
-
+// create report for human readable
+// this is the custom part for us to deploy the report
 echo 'Testing class SimpleTester within result:<br/>';
 $no = 0;
 foreach ($tester->result() as $subtest => $tests) {
@@ -26,5 +28,5 @@ foreach ($tester->result() as $subtest => $tests) {
 		return $test === false;
 	}));
 	$no++;
-	echo "$no. <b>$subtest:</b> $tested tests ($passed passed, $failed failed).<br/>";
+	echo "$no. $subtest: $tested tests ($passed passed, $failed failed).<br/>";
 }
