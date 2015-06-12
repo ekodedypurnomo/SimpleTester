993fc629ee1fcfa7f6c92611f36b8441277750be
Eko Dedy <eko.dedy.purnomo@gmail.com>
2015-06-12 18:20:16 +0700

    Improve example for more kiss

--- example.php
+++ example.php
@@ -1,32 +1,42 @@
 <?php
 
+// sample class to test
+class SampleClass{
+	private $book = 'a novel';
+	public $written = null;
+	function read(){
+		return 'reading '.$this->book;
+	}
+	function write($value = null){
+		$this->written = $value;
+		return 'writing '.$value;
+	}
+}
+
+
+// simply require then test
 require 'SimpleTester.php';
 
-// begin the test
 $tester = new SimpleTester;
-// test grup 1
-$tester->subtest('Test SimpleTester class', function($subtest) use($tester){
-    $subtest->test('must have a name', property_exists($tester, 'name'));
-    $subtest->test('must have a templateReport', property_exists($tester, 'templateReport'));
-});
-// test grup 2
-$tester->subtest('Test SimpleSubtester class', function($subtest){
-    $subtest->test('must have a name', empty($subtest->name) === false);
-    $subtest->test('must have a tester', empty($subtest->tester) === false);
+
+$tester->subtest('Test for SampleClass', function($subtest) use($tester){
+	$obj = new SampleClass;
+	$subtest->test('must reading a novel', $obj->read() == 'reading a novel');
+	$subtest->test('must writing "something"', $obj->write('something') == 'writing something');
+	$subtest->test('"something" should be keep on property', $obj->written == 'something');
 });
 
-// create report for human readable
-// this is the custom part for us to deploy the report
-echo 'Testing class SimpleTester within result:<br/>';
-$no = 0;
-foreach ($tester->result() as $subtest => $tests) {
-	$tested = count($tests);
-	$passed = count(array_filter($tests, function($test){
-		return $test === true;
-	}));
-	$failed = count(array_filter($tests, function($test){
-		return $test === false;
-	}));
-	$no++;
-	echo "$no. $subtest: $tested tests ($passed passed, $failed failed).<br/>";
+
+// how about result?
+// create with your own style
+echo 'Result:<br/>';
+foreach ( $tester->getSubtests() as $subtest) {
+	$subtestName = $subtest->getName();
+	$tested = $subtest->countTests();
+	$passed = $subtest->countPassed();
+	$failed = $subtest->countFailed();
+	echo "<b> $subtestName: $tested tests ($passed passed, $failed failed). </b><br/>";
+	foreach ($subtest->getTests() as $testName => $result) {
+		echo "* $testName is => ".($result ? 'Success' : 'Failed')."<br/>";
+	}
 }
